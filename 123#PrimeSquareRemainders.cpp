ac14b64a696be237e0e3c383f1b0db0129695a24
HeeM <gmlaud0209@gmail.com>
2014-04-14 01:06:07 +0900

    123번 품

--- 123#PrimeSquareRemainders.cpp
+++ 123#PrimeSquareRemainders.cpp
@@ -1,31 +1,30 @@
 #include<stdio.h>
+#include "library/prime/prime.h"
 
-long long Test(long long a, int& b)
+long long Test(long long a, long long n)
 {
-	long long max = 0;
-	for(long long i = 1; i < 2 * a; i += 2)
-	{
-		//printf("a*i : %d, 나머지:%d\n", 2*a*i, (2*a*i)%(a*a));
-		long long x = ((2*a*i) % (a*a));
-		if( max < x )
-		{
-			max = x;
-			b = i;
-		}
-	}
-	return max;
+	if( n % 2 == 0 )
+		return 2;
+	else
+		return ((2*a*n) % (a*a));
 }
 
 int main()
 {
-
-	long long sum = 0;
-	for(long long i = 3; i <= 1000; ++i)
+	HeeM::Primes p;
+	p.Init(6);
+	long long answer = 0;
+	const auto& pVec = p.GetPrimes();
+	for(size_t iter = 0; iter < pVec.size(); ++iter)
 	{
-		int x;
-		int t = Test(i, x);
-		printf("Test(%d) : %d.. (%d^%d + %d^%d) %% %d == %d\n", i, t, i-1, x, i+1, x, i*i, t);
-		sum += t;
+		long long i = pVec[iter];
+		long long t = Test(i, (long long)(iter+1));
+		printf("Test(%lld) : %lld.. (%lld^%lu + %lld^%lu) %% %lld == %lld\n", i, t, i-1, iter+1, i+1, iter+1, i*i, t);
+		if( t > 10000000000 )
+		{
+			answer = iter + 1;
+			break;
+		}
 	}
-	printf("Answer:%lld\n", sum);
+	printf("Answer:%lld\n", answer);
 }
